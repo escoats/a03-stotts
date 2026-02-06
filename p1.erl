1e62ca2cec97e0c4fdbd8e36aafda3a2b0a40908
Elizabeth Coats <escoats@unc.edu>
2026-02-06 11:26:19 -0500

    Set up p1

---
+++ p1.erl
@@ -0,0 +1,17 @@
+-module(p1).
+-export([p1/1]).
+
+% TODO: take N as input from user
+p1(N) when not is_integer(N) ->
+    io:fwrite("not an integer\n");
+p1(N) when N < 0 ->
+    io:fwrite("~p~n", [math:pow(abs(N), 7)]);
+p1(N) when N == 0 ->
+    io:fwrite("~p~n", [N]);
+p1(N) when N > 0 ->
+    if
+        (N rem 7) == 0 ->
+            io:fwrite("~p~n", [math:pow(N, 0.2)]);
+        true ->
+            io:fwrite("todo: implement...\n")
+    end.
